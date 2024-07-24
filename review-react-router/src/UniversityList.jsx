651b40c73eb453f0f26a9043e06f44625c644b37
Izumi-Konata <yjzhu981001@qq.com>
2024-07-24 23:59:42 +1200

    add search

--- review-react-router/src/UniversityList.jsx
+++ review-react-router/src/UniversityList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './css/UniversityList.css';
 import UniversitySquare from './Component/UniversitySquare';
+import { SlMagnifier } from "react-icons/sl";
 
 function UniversityList() {
   return (
@@ -8,7 +9,9 @@ function UniversityList() {
       <p className="UniversityTitle">Universities</p>
       <div className="SearchUniArea">
         <input className="SearchUni"></input>
-        <button className="SearchButton">Search</button>
+        <button className="SearchUniButton">
+            <SlMagnifier />
+        </button>
       </div>
       <div className="UniversityGrid">
         <UniversitySquare />
